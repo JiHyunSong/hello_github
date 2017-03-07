0106d73888a9335c816bddeb0de574dff4eb4631
JiHyun Song <jihyun.song0829@gmail.com>
2017-03-07 23:43:21 +0900

    b return variable through parameter

--- simple.py
+++ simple.py
@@ -5,12 +5,11 @@ def a():
     a = 1
     return a
 
-def b():
-    return 'b'
-
+def b(var):
+    return var
 
 if __name__ == "__main__":
     print "hello world!"
     print a()
-    print b()
+    print b(3)
 
