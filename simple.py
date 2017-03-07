15b1cd7a22e2d5a4d41fbd16493046f2e4c85199
JiHyun Jenny Song <jihyun.song0829@gmail.com>
2017-03-08 00:12:54 +0900

    Revert "change a , b"

--- simple.py
+++ simple.py
@@ -2,13 +2,14 @@
 
 
 def a():
-    a = 1
-    return a
+    return 'a'
 
-def b(var):
-    return var
+def b():
+    return 'b'
+
 
 if __name__ == "__main__":
     print "hello world!"
     print a()
-    print b(3)
+    print b()
+
